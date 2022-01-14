703335c6e1f5511cbfba4d2998e84289310da1f2
PavelDermichev1998 <83948999+PavelDermichev1998@users.noreply.github.com>
2022-01-14 14:05:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const item = document.querySelector(".item");
 const placeholders = document.querySelectorAll(".placeholder");
-
 item.addEventListener("dragstart", dragstart);
 item.addEventListener("dragend", dragend);
 
